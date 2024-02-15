6e3cf6555b26b9feefff08027fedc9c764248321
suportemaxxi <suportemaxxi@SUPORTE-MAXXI>
2024-02-15 17:38:13 -0400

    Adicionar arquivos de projeto.

---
+++ BlazorCalendario/Data/EventoDia.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorCalendario.Data
+{
+    public class EventoDia
+    {
+        public int Dia { get; set; }
+        public string Nota { get; set; } = "";
+        public DateTime DataEvento { get; set; } = new DateTime(1900, 1,1);
+        public DateTime DataInicial { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime DataFinal { get; set; } = new DateTime(1900, 1, 1);
+        public string ValorData { get; set; } = "";
+        public string NomeDia { get; set; } = "";
+        public string Mensagem { get; set; } = "";
+        public string Atendente { get; set; } = "";
+        public int CodAtendente { get; set; }
+        public string Cliente { get; set; } = "";
+        public int CodEmp { get; set; }
+    }
+}
---
+++ BlazorCalendario/Data/Semana.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorCalendario.Data
+{
+    public class Semana
+    {
+        public List<EventoDia> Dates { get; set; } = new List<EventoDia>();
+    }
+}
