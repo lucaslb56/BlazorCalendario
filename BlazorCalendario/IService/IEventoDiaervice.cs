9338bca4b065b5034559e2c43aa2c2ce9265cbfe
Higor Rafael <higorrafael390@gmail.com>
2024-02-16 10:36:50 -0400

    Adiciona espaço fixa na tebela e conexão com banco de dados

---
+++ BlazorCalendario/IService/IEventoDiaervice.cs
@@ -0,0 +1,12 @@
+﻿using BlazorCalendario.Data;
+
+namespace BlazorCalendario.IService
+{
+    public interface IEventoDiaervice
+    {
+        EventoDia SalvarOuAtualizar(EventoDia eventoDia);
+        EventoDia GetEvento(DateTime dataEvento);
+        List<EventoDia> GetEvents(DateTime dataInicial, DateTime dataFinal);
+        string Delete(int id);
+    }
+}
---
+++ BlazorCalendario/Service/EventoDiaervice.cs
@@ -0,0 +1,28 @@
+﻿using BlazorCalendario.Data;
+using BlazorCalendario.IService;
+
+namespace BlazorCalendario.Service
+{
+    public class EventoDiaervice : IEventoDiaervice
+    {
+        public string Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventoDia GetEvento(DateTime dataEvento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EventoDia> GetEvents(DateTime dataInicial, DateTime dataFinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventoDia SalvarOuAtualizar(EventoDia eventoDia)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
